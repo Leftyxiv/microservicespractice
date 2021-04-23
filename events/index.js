d5f0bcb9851e9dcfa7d07f8a0939b44f01428cce
Leftyxiv <manny@aledoux.net>
2021-04-23 09:41:17 -0400

    got all services deployed

--- events/index.js
+++ events/index.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
 
 const events = [];
 
-app.post('/events', (req, res) => {
+app.post("/events", (req, res) => {
   const event = req.body;
   events.push(event);
 
-  axios.post('http://posts-clusterip-srv:9000/events', event)
-  // axios.post('http://localhost:9001/events', event)
-  // axios.post('http://localhost:9002/events', event)
-  // axios.post('http://localhost:9003/events', event)
+  axios.post("http://posts-clusterip-srv:9000/events", event);
+  axios.post("http://comments-srv:9001/events", event);
+  axios.post("http://query-srv:9002/events", event);
+  axios.post("http://moderation-srv:9003/events", event);
 
-res.send({ status: 'ok' })
-})
+  res.send({ status: "ok" });
+});
 
-app.get('/events', (req, res) => {
-  res.send(events)
-})
+app.get("/events", (req, res) => {
+  res.send(events);
+});
 
 app.listen(9005, () => {
-  console.log('listening for events on 9005')
-})
+  console.log("listening for events on 9005");
+});
